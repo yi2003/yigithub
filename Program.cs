20f3482dfe96fc5d42394b3dd6ca082a40fbe58d
unknown <zhangyi@ymt.corp>
2016-06-24 09:50:25 +0800

    test git

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+namespace aspnetcoreapp
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+
+        .UseStartup<Startup>()
+        .UseUrls("http://localhost:5033")
+        .Build();
+
+      host.Run();
+    }
+  }
+}
